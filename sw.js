4f6e7e26521a715ee2d121bd195b19b25a43fc97
Salva <salvadorsolealvarez@gmail.com>
2026-04-21 11:30:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "kaboom-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./Kaboom.htm",
+        "./preguntas.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
